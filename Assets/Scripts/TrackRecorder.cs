688eff92c55c42f050b30e5abc0f2924c8964e2f
Surov Vladimir <vovkasu@gmail.com>
2023-01-20 15:22:20 +0200

    Added Camera rotation

--- Assets/Scripts/TrackRecorder.cs
+++ Assets/Scripts/TrackRecorder.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace CameraTrajectoryRecorder
 {
-    public class TrackRecorder : MonoBehaviour
+    public class TrackRecorder : MonoBehaviour, IPointerMoveHandler, IPointerDownHandler, IPointerUpHandler
     {
+        public Transform CameraRotationContainer;
+        public float RotationSensitivity = 0.25f;
+        public bool IsPointerDown;
         public bool InProcess { get; private set; }
         public CameraTrack StartRecording(Camera mainCamera, CameraTrack cameraTrack)
         {
@@ -27,5 +31,33 @@ namespace CameraTrajectoryRecorder
         {
             InProcess = false;
         }
+
+        #region User manipulations
+        public void OnPointerMove(PointerEventData eventData)
+        {
+            if (IsPointerDown && InProcess)
+            {
+                var delta = eventData.delta * RotationSensitivity;
+                var sourceEulerAngles = CameraRotationContainer.rotation.eulerAngles;
+                var eulerAngles = new Vector3(
+                    sourceEulerAngles.x - delta.y, 
+                    sourceEulerAngles.y + delta.x,
+                    sourceEulerAngles.z);
+                
+                Debug.Log($"OnPointerMove:{delta} eulerAngles:{eulerAngles}");
+                CameraRotationContainer.rotation = Quaternion.Euler(eulerAngles);
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            IsPointerDown = true;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            IsPointerDown = false;
+        }
+        #endregion
     }
 }
